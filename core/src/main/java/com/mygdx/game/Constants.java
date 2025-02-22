3a28697d93fc9bc72303c821a12612fa28c567f9
Rokas <rdidrikas@gmail.com>
2025-02-22 17:58:32 +0200

    Last working commit

---
+++ core/src/main/java/com/mygdx/game/Constants.java
@@ -0,0 +1,5 @@
+package com.mygdx.game;
+
+public class Constants {
+    public static final float PPM = 32; // 32 pixels = 1 meter
+}
--- core/src/main/java/com/mygdx/game/Player.java
+++ core/src/main/java/com/mygdx/game/Player.java
@@ -3,9 +3,14 @@ package com.mygdx.game;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Rectangle;
+
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class Player {
 
