7e3d471101af4e3c642a5de3973c84367be998e2
Rokas <rdidrikas@gmail.com>
2025-03-13 18:18:54 +0200

    Added a simple play and exit menu

--- core/src/main/java/com/mygdx/game/MenuState.java
+++ core/src/main/java/com/mygdx/game/MenuState.java
@@ -3,37 +3,97 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MenuState extends GameState {
-    private Texture menuTexture;
-    // FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("Fonts/Font/MenuFont.ttf"));
+    private Stage stage;
+    private Skin skin;
+    private BitmapFont customFont;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
-        menuTexture = new Texture("menu_screen.png");
+        createFont();
+        createUI();
+    }
 
+    private void createFont() {
+        // Load and generate custom font
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(
+            Gdx.files.internal("Fonts/Font/MenuFont.ttf")
+        );
+        FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        params.size = 80; // Font size in pixels
+        customFont = generator.generateFont(params);
+        generator.dispose();
+    }
+
+    private void createUI() {
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        // Create skin and configure styles
+        skin = new Skin();
+        skin.add("default-font", customFont);
+
+        // Configure button style
+        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+        buttonStyle.font = skin.getFont("default-font");
+
+        skin.add("default", buttonStyle);
+
+        // Create table layout
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        // Create UI elements
+        TextButton playButton = new TextButton("Play", skin);
+        TextButton exitButton = new TextButton("Exit", skin);
+
+        // Add button listeners
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                gsm.setState(new PlayingState(gsm));
+            }
+        });
+
+        exitButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+            }
+        });
+
+        // Arrange UI elements
+        table.add(playButton).padBottom(20).row();
+        table.add(exitButton).padBottom(20).row();
     }
 
     @Override
     public void update(float delta) {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
-            gsm.setState(new PlayingState(gsm));
-        }
+        stage.act(delta);
     }
 
     @Override
     public void render(SpriteBatch batch) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(menuTexture, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.end();
+        stage.draw();
     }
 
     @Override
     public void dispose() {
-        menuTexture.dispose();
+        stage.dispose();
+        skin.dispose();
+        customFont.dispose();
     }
 }
